c663f9d7965722f0c459b469d88bfaabfa507e94
Paul Durrant <paul.durrant@citrix.com>
2015-07-08 15:43:23 +0100

    Re-base to 31bceb134af626578208f5fc95545d24c3b0932e
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- patchqueue.py
+++ patchqueue.py
@@ -2,7 +2,7 @@ remoterepo =  "git://xenbits.xen.org/pvdrivers/win/xeniface.git"
 baserepo = "git://hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 baserepopush = "ssh://xenhg@hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 
-basetag = "30b088af45d9d10566d83553e533c3c8c3484d16"
+basetag = "31bceb134af626578208f5fc95545d24c3b0932e"
 
 package = "xeniface"
 
