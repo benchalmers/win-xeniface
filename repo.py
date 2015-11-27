7aee708ce0886d0d993c1760cebf5eaa1f7c139d
Paul Durrant <paul.durrant@citrix.com>
2015-11-27 10:15:29 +0000

    Update to 8.1.0-rc5
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- repo.py
+++ repo.py
@@ -2,5 +2,5 @@ upstream = r"git://xenbits.xen.org/pvdrivers/win/xeniface.git"
 internal_push = r"ssh://xenhg@hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 internal_pull = r"git://hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 
-ref = "8.1.0-rc4"
+ref = "8.1.0-rc5"
 patches = []
