6cbc983f5cdec8b5e2d58a341e8c07dabfec5c8d
Ben Chalmers <Ben.Chalmers@citrix.com>
2016-01-12 15:36:19 +0000

    CP-15479 : Debrand lite agent icon

--- repo.py
+++ repo.py
@@ -3,4 +3,6 @@ internal_push = r"ssh://xenhg@hg.uk.xensource.com/closed/windows/xenbits/win-xen
 internal_pull = r"git://hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 
 ref = "8.1.0-rc6"
-patches = []
+patches = [
+        '0001-CP-15479-Debrand-lite-agent.patch',
+        ]
