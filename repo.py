069df956fceb572bc69f3d072e8b18f774a02e79
Paul Durrant <paul.durrant@citrix.com>
2016-12-12 15:30:08 +0000

    Use staging-8.2 branch
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- repo.py
+++ repo.py
@@ -2,7 +2,7 @@ upstream = r"git://xenbits.xen.org/pvdrivers/win/xeniface.git"
 internal_push = r"ssh://xenhg@hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 internal_pull = r"git://hg.uk.xensource.com/closed/windows/xenbits/win-xeniface.git"
 
-ref = "master"
+ref = "staging-8.2"
 patches = [
         '0001-Add-write-to-data-updated-to-wake-XAPI.patch',
         '0002-Also-check-platform-timeoffset.patch',
